b9a1c91efdcf0523339ced5cb3d8d032f3d22936
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-08-31 17:30:57 +0200

    Social icons visual update, Mobile menu fixed to close on any item tap

--- src/js/modules/MobileMenu.js
+++ src/js/modules/MobileMenu.js
@@ -3,8 +3,11 @@ var $ = require("jquery");
 function MobileMenu() {
     var $mainMenu = $(".main-menu");
     var $menuIcon = $(".menu-icon");
+    var $menuLinks = $(".main-menu__list a");
 
     $menuIcon.on("click", toggleMenu);
+    $menuLinks.on("click", toggleMenu);
+    $menuLinks.on("tap", toggleMenu);
 
     function toggleMenu() {
         $mainMenu.toggleClass("main-menu--shown");
