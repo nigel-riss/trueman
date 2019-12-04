ff5abcde701b0de8ce7b76a8f9b1d11fa369bdfb
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-12-04 22:27:10 +0200

    Add new map markers

--- dist/map.js
+++ dist/map.js
@@ -1,8 +1,14 @@
 ;
 function initMap() {
+    var mapZoom = 13;
+    if (document.body.clientWidth > 1000) {
+        mapZoom = 14;
+    }
+
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 17,
-        center: {lat: 53.9132898, lng: 27.5811282},
+        zoom: mapZoom,
+        // center: {lat: 53.9132898, lng: 27.5811282},
+        center: {lat: 53.91157521592535, lng: 27.56732355677787},
         disableDefaultUI:true,
         scrollwheel: false,
         zoomControl: true,
@@ -11,6 +17,13 @@ function initMap() {
 
     var marker = new google.maps.Marker({
         map: map,
-        position: {lat: 53.9132898, lng: 27.5811282}
+        position: {lat: 53.9132898, lng: 27.5811282},
+        icon: 'favicon-32x32.png'
+    });
+
+    var marker2 = new google.maps.Marker({
+        map: map,
+        position: {lat: 53.90767807065405, lng: 27.549721499999972},
+        icon: 'favicon-32x32.png'
     });
 }
